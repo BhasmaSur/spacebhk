09e96bdadefdbc1304698b978dd91b206958609c
mradulmishra <rakshashltd@gmail.com>
2022-10-02 12:31:02 +0530

    kumar-properties

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import LandingPage from './views/landing-page';
+import KumarProperties from './views/kumar-properties';
+import ErrorPage from './views/error-page';
 function App() {
   return (
     <Router>
     <Routes>
+      <Route path='/kumar-properties' element={<KumarProperties />} exact/>
       <Route path='/' element={<LandingPage />} exact/>
+      <Route path='/*' element={<ErrorPage />} exact/>
     </Routes>
   </Router>
   );

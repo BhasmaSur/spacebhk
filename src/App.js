7d4122f95c629b553e98d39aa48b2c232323d471
mradulmishra <rakshashltd@gmail.com>
2023-12-04 21:43:55 +0530

    space finder

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import LandingPage from './views/landing-page';
 import KumarProperties from './views/kumar-properties';
 import ErrorPage from './views/error-page';
+import PrivacyPolicy from './views/privacy-policy';
 function App() {
   return (
     <Router>
     <Routes>
       <Route path='/kumar-properties' element={<KumarProperties />} exact/>
+      <Route path='/privacy-policy' element={<PrivacyPolicy />} exact/>
       <Route path='/' element={<LandingPage />} exact/>
       <Route path='/*' element={<ErrorPage />} exact/>
     </Routes>
